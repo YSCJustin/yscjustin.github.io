595e3f0a3d7af2e5ef325b9b4cd91bad09c079ab
YSCJustin <yscjustin@gmail.com>
2023-08-15 01:29:20 +0800

    MEGA CHANGES

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
-window.onload(() => {
-    alert('Hello World!');
-})
+function checkAElement() {
+    var aElement = document.querySelector('a');
+    if (aElement) {
+        setTimeout(function() {
+            alert("Hello World!");
+        }, 50); 
+    } else {
+        setTimeout(checkAElement, 50);
+    }
+}
+
+checkAElement(); 
