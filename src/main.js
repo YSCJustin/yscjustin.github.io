6de90e27208bc36c4b30013de5284496405bac3b
YSCJustin <yscjustin@gmail.com>
2023-08-15 14:26:00 +0800

    ARE YOU KIDDING ME

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
+const menuBtn = document.querySelector('.menu-btn');
+const sidebar = document.querySelector('.sidebar');
+const content = document.querySelector('.content');
+
+menuBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('open');
+  menuBtn.classList.toggle('open');
+  content.classList.toggle('shift');
+});
 function checkAElement() {
     var aElement = document.querySelector('a');
     if (aElement) {
         setTimeout(function() {
-            alert("Hello World!");
+            alert("real!");
         }, 50); 
     } else {
         setTimeout(checkAElement, 50);
@@ -10,3 +19,4 @@ function checkAElement() {
 }
  
 checkAElement(); 
+
