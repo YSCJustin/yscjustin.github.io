1893b4ef26bc47fb9b0a9a15c2ba28831373d66c
YSCJustin <yscjustin@gmail.com>
2023-08-29 00:15:45 +0800

    Easter is here

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", async () => {
+
+
+let words = [];
+let formats = ["js", "json", "html", "css", "php", "cpp", "md", "txt", "py", "java", "c", "ts", "tsx", "jsx", "rb", "go"]
+  await fetch('words.json').then(response => response.json()).then(data => words = data);
+
     const year = document.querySelector("#year")
     const date = new Date()
     year.innerHTML = date.getFullYear()
@@ -8,13 +14,14 @@ document.addEventListener("DOMContentLoaded", async () => {
     const randomButton = document.querySelector("#randomButton")
   const delay = ms => new Promise(res => setTimeout(res, ms));
     async function progressUpdate(){
-        const time = Math.floor(Math.random() * 4) + 2
+        const time = Math.floor(Math.random() * 3) + 2
       setTimeout(async () => {
-        const random = Math.floor(Math.random() * 15)
+        const random = Math.floor(Math.random() * 20)+1
         if(progressBar.value < 100 && progressBar.hasAttribute("value")){
             progressBar.value += random
-
-            progressBarSpan.innerHTML = progressBar.value + "%"
+          let randomWord = words[Math.floor(Math.random() * words.length)]
+          let format = formats[Math.floor(Math.random() * formats.length)]
+            progressBarSpan.innerHTML = progressBar.value + "% " + `Downloading   <strong>${randomWord.toUpperCase()}.${format}</strong>`
             if(progressBar.value >= 100){
                 progressBarSpan.innerHTML = ""
                 progressBar.removeAttribute("value")
